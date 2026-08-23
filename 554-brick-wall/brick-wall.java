c163d5933d0f3f49c5da69b90ba713b134d61fa3
Vidit <viditmalyan@gmail.com>
2026-08-23 23:26:58 +0530

    Time: 12 ms (95.34%) | Memory: 49.8 MB (67.06%) - LeetSync

---
+++ 554-brick-wall/brick-wall.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int leastBricks(List<List<Integer>> wall) {
+        Map<Integer, Integer> edgeCount = new HashMap<>();
+        int maxEdges = 0;
+
+        for (List<Integer> row : wall) {
+
+            int position = 0;
+
+            // Skip the final edge of each row
+            for (int i = 0; i < row.size() - 1; i++) {
+
+                position += row.get(i);
+
+                int count = edgeCount.getOrDefault(position, 0) + 1;
+
+                edgeCount.put(position, count);
+
+                maxEdges = Math.max(maxEdges, count);
+            }
+        }
+        return wall.size() - maxEdges;
+    }
+}
